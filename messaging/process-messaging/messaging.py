28a3aae08c4cb15bf8da2ae9283ad537a3f57c25
SrinathReddy93 <srinathreddy088@gmail.com>
2022-12-04 18:49:11 +0530

    send message between process

---
+++ messaging/process-messaging/messaging.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process, Queue
+import time
+
+def consumer(q):
+    while True:
+        txt = q.get()
+        print(txt)
+        # time.sleep(1)
+
+def producer(q):
+    while True:
+        q.put([1,2])
+        print('message sent')
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    q = Queue(maxsize=10)
+    p1 = Process(target=consumer, args=(q,))
+    p2 = Process(target=producer, args=(q,))
+    p1.start()
+    p2.start()
+    print('In main')
+    time.sleep(1)
+    
